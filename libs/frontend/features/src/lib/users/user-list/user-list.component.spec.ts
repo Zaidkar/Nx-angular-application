bce7e115526395702560ca1d2768a7d8758859d0
Zaid Karmoudi <zaidkarmoudi@gmail.com>
2024-11-28 13:07:10 +0100

    redeploy attempt

--- libs/frontend/features/src/lib/users/user-list/user-list.component.spec.ts
+++ libs/frontend/features/src/lib/users/user-list/user-list.component.spec.ts
@@ -14,8 +14,4 @@ describe('UserListComponent', () => {
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
-
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
 });
