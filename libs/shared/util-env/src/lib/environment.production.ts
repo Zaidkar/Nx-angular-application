de4aa894ee92187bc334b0e1a239cba36ad41389
Zaid Karmoudi <zaidkarmoudi@gmail.com>
2024-11-28 14:09:58 +0100

    updated api url to online backend

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -3,8 +3,10 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: true,
 
-    ROOT_DOMAIN_URL: 'https://nxworkshop.azurewebsites.net',
-    dataApiUrl: 'https://nxworkshop.azurewebsites.net/api',
+    ROOT_DOMAIN_URL:
+        'https://nx-angular-data-api-fuewedhvf8hxcqbq.westeurope-01.azurewebsites.net',
+    dataApiUrl:
+        'hhttps://nx-angular-data-api-fuewedhvf8hxcqbq.westeurope-01.azurewebsites.net/api',
 
     MONGO_DB_CONNECTION_STRING:
         'mongodb+srv://nx:nxpassword@nx-applicatie.vs6sa.mongodb.net/nx-angular?retryWrites=true&w=majority&appName=Nx-angular'
