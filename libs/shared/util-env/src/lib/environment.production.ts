ef3607e9f19acf559fdf2f9b84760c0eae6487ce
Zaid Karmoudi <150630253+Zaidkar@users.noreply.github.com>
2024-11-28 16:08:01 +0100

    fix typo in url

--- libs/shared/util-env/src/lib/environment.production.ts
+++ libs/shared/util-env/src/lib/environment.production.ts
@@ -2,12 +2,10 @@ import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
     production: true,
-
     ROOT_DOMAIN_URL:
         'https://nx-angular-data-api-fuewedhvf8hxcqbq.westeurope-01.azurewebsites.net',
     dataApiUrl:
-        'hhttps://nx-angular-data-api-fuewedhvf8hxcqbq.westeurope-01.azurewebsites.net/api',
-
+        'https://nx-angular-data-api-fuewedhvf8hxcqbq.westeurope-01.azurewebsites.net/api',
     MONGO_DB_CONNECTION_STRING:
         'mongodb+srv://nx:nxpassword@nx-applicatie.vs6sa.mongodb.net/nx-angular?retryWrites=true&w=majority&appName=Nx-angular'
 };
