909c21f6d5a73fa2e0d3f64ac097467e7bdca1c9
Zaid Karmoudi <150630253+Zaidkar@users.noreply.github.com>
2024-11-28 23:48:07 +0100

    update game list attribute name

--- libs/shared/util-env/src/lib/environment.ts
+++ libs/shared/util-env/src/lib/environment.ts
@@ -3,8 +3,10 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
     production: false,
 
-    ROOT_DOMAIN_URL: 'http://localhost:3000',
-    dataApiUrl: 'http://localhost:3000/api',
+    ROOT_DOMAIN_URL:
+        'https://nx-angular-data-api-fuewedhvf8hxcqbq.westeurope-01.azurewebsites.net',
+    dataApiUrl:
+        'https://nx-angular-data-api-fuewedhvf8hxcqbq.westeurope-01.azurewebsites.net/api',
 
     MONGO_DB_CONNECTION_STRING:
         'mongodb+srv://nx:nxpassword@nx-applicatie.vs6sa.mongodb.net/nx-angular?retryWrites=true&w=majority&appName=Nx-angular'
